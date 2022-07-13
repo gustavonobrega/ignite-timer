df165fb38b994f87b304fe38a217df9080c363d2
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-07-13 20:00:27 -0300

    Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
 
 import { defaultTheme } from './styles/themes/default';
 
@@ -7,6 +8,7 @@ export function App() {
     <ThemeProvider theme={defaultTheme} >
       <h1>Hello World</h1>
 
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
